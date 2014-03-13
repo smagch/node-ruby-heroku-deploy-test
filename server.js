4e55309281700cabc322ea5c7bef83104428af58
smagch <smagch@gmail.com>
2014-03-13 14:48:04 +0900

    add examples

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 // Mock server to showcase react app
 // This server shouldn't be deployed in production
-var http = require('http');
 var express = require('express');
 var path = require('path');
 var app = express();
 
+app.set('port', Number(process.env.PORT) || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'assets')));
-// return project details
+
 app.get('/', function (req, res) {
-  res.send('<html>Hello World. CSS is <a href="/css/style.css">here</a></html>');
+  res.render('home');
 });
 
-var port = Number(process.env.PORT || 5000);
-http.createServer(app).listen(port, function() {
-  console.log('server listen on PORT 5000');
+app.get('/product', function (req, res) {
+  res.render('product');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('server listen on PORT %d', app.get('port'));
 });
